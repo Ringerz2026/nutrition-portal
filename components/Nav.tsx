3148c143155d5cd8b222d552cf6871387d851895
Thomas Ringrose <thomas.ringrose@gmail.com>
2026-07-01 09:17:19 +0100

    Add separate admin links for articles and users

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,7 +14,8 @@ export default function Nav({ lang = 'en' }: { lang?: Lang }) {
       <div className="navlinks">
         <Link href={`/dashboard?lang=${lang}`}>{t.dashboard}</Link>
         <Link href={`/account?lang=${lang}`}>Account</Link>
-        <Link href={`/admin/users?lang=${lang}`}>{t.admin}</Link>
+        <Link href={`/admin?lang=${lang}`}>Articles</Link>
+        <Link href={`/admin/users?lang=${lang}`}>Users</Link>
 
         <Link className="button ghost" href={`?lang=${toggleLang}`}>
           {toggleLang.toUpperCase()}
